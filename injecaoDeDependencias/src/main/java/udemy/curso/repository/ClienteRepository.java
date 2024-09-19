5175aecd928ec4a861e7e652cf299ac3ba225e47
acmors <marcosantoniosilva047@gmail.com>
2024-09-19 20:04:51 -0300

    first

---
+++ injecaoDeDependencias/src/main/java/udemy/curso/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package udemy.curso.repository;
+
+import org.springframework.stereotype.Repository;
+
+import udemy.curso.model.Cliente;
+
+@Repository
+public class ClienteRepository {
+	
+	public void persistir(Cliente cliente) {
+		
+	}
+}
