ab64e7bdaf8ec7e2579c176c527fcb51f3080c6e
acmors <marcosantoniosilva047@gmail.com>
2024-09-30 05:26:47 -0300

    metodos deletar

--- injecaoDeDependencias/src/main/java/udemy/curso/InjecaoDeDependenciasApplication.java
+++ injecaoDeDependencias/src/main/java/udemy/curso/InjecaoDeDependenciasApplication.java
@@ -18,12 +18,48 @@ public class InjecaoDeDependenciasApplication {
 	public CommandLineRunner init(@Autowired ClientesRepository clientes) {
 		return args -> {
 			
+			System.out.println("Salvandor clientes");
 			clientes.salvar(new Cliente("Marcos"));
 			clientes.salvar(new Cliente("Pedro"));
-			clientes.salvar(new Cliente("Lucas"));
+			clientes.salvar(new Cliente("LucasCliente"));
 			
 			List<Cliente> todosClientes = clientes.obterTodos();
 			todosClientes.forEach(System.out::println);
+			System.out.println();
+			
+			System.out.println("atualizando clientes....");
+			todosClientes.forEach(c -> {
+				c.setNome(c.getNome() + " atualizado.");
+				clientes.atualizar(c);
+			});
+			
+			System.out.println("Obtendo clientes atualizados.");
+			todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+			System.out.println();
+			
+			System.out.println("Buscando clientes");
+			clientes.buscarPorNome("Cli").forEach(System.out::println);;
+			
+			System.out.println("deletando clientes.");
+			
+			clientes.obterTodos().forEach(c -> {
+				clientes.deletar(c.getId());
+			});
+			
+			todosClientes = clientes.obterTodos();
+			if(todosClientes.isEmpty()) {
+				System.out.println("nenhum cliente encontrado.");
+			}else {
+				todosClientes.forEach(System.out::println);
+			}
+			
+			System.out.println();
+			
+			System.out.println("Obtendo todos os clientes.");
+			todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+
 		};
 	}
 
--- injecaoDeDependencias/src/main/java/udemy/curso/repository/ClientesRepository.java
+++ injecaoDeDependencias/src/main/java/udemy/curso/repository/ClientesRepository.java
@@ -16,7 +16,9 @@ public class ClientesRepository {
 
 	private static String INSERT = "insert into clientes (nome) values (?)";
 	private static String SELECT_ALL = "SELECT * FROM CLIENTES";
-	private static String UPDATE = "update cliente set nome = ? where id = ?";
+	private static String UPDATE = "update clientes set nome = ? where id = ?";
+	private static String DELETE = "delete from clientes where id = ?";
+	private static String SELECT_POR_NOME = "SELECT * FROM CLIENTES WHERE NOME LIKE ?";
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
@@ -37,11 +39,29 @@ public class ClientesRepository {
 		}); 
 	}
 	
+	private RowMapper<Cliente> obterClienteMapper(){
+		return new RowMapper<Cliente>() {
+			@Override
+			public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+				Integer id = resultSet.getInt("id");
+				String nome = resultSet.getString("nome");
+				return new Cliente(nome, id);
+			}
+		};
+	}
+	
 	public Cliente atualizar(Cliente cliente) {
 		
-		jdbcTemplate.update(null)
-		
+		jdbcTemplate.update(UPDATE, new Object[]{cliente.getNome(), cliente.getId()});
 		return cliente;
+	}
+	
+	public void deletar(Integer id) {
+		jdbcTemplate.update(DELETE, new Object[] {id});
+	}
+	
+	public List<Cliente> buscarPorNome(String nome) {
 		
+		return jdbcTemplate.query(SELECT_POR_NOME, obterClienteMapper(), "%" + nome + "%");
 	}
 }
